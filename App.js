63c263a33cc227ffb1a671d66fcbc86f755ccde7
Sefa-Git <Sefa@superapplabs.co>
2024-07-07 16:52:31 +0300

    added background color to meals over view

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View,Text, Pressable} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+
+import CategoresScreen  from './screens/CategoriesScreen';
+import MealsOverViewScreen from './screens/MealsOverViewScreen';
+
+const Stack = createNativeStackNavigator();
+
+/* 
+*/
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+<>
+
+  <StatusBar style='dark'/>    
+  
+  <NavigationContainer>
+        
+        <Stack.Navigator> 
+        
+          <Stack.Screen name="mealsCategories" component={CategoresScreen}/>
+          <Stack.Screen name="mealsOverview" component={MealsOverViewScreen}/>
+        
+        </Stack.Navigator>
+  </NavigationContainer>
+
+</>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container0: {
     flex: 1,
+    borderWidth:20,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container1: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height:100,
+    width:100,
+    elevation:1,
+    borderRadius:9,
+  },
 });
+
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,16 +1,36 @@
 import { FlatList } from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
 
 
-function renderCategoryItem(item){
-    return;
-}
-
-
-function CategoresScreen(){
-
- return <FlatList data={CATEGORIES} keyExtractor={(item) => item.id} />
- 
+function CategoresScreen({navigation}){
+    
+    function renderCategoryItem(itemData){
+        
+        function pressHandler(){
+            navigation.navigate("mealsOverview",{
+                categoryId: itemData.item.id, 
+                buttonColor: itemData.item.color,
+                });
+            }
+        
+        return( 
+            <CategoryGridTile 
+            title={itemData.item.title} 
+            color={itemData.item.color} 
+            onPress={pressHandler}
+            />
+        );
+    }
+    
+ return (
+    <FlatList 
+    data={CATEGORIES} 
+    keyExtractor={(item) => item.id} 
+    renderItem={renderCategoryItem}
+    numColumns={2}
+    />
+ );
  
 }
 
